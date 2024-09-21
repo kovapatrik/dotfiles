8702e607d55c236154e091d254fb7affb787e06b
Patrik Kovalovszky <kovapatrik@gmail.com>
2024-09-21 18:02:09 +0200

    feat: some macos defaults, brew

--- .setup.sh
+++ .setup.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+# This script installs everything from scratch. It is meant to be used through a curl to bash command.
+
+# Install XCode Command Line Tools if necessary
 xcode-select --install || echo "XCode already installed"
 
 if which -s brew; then
